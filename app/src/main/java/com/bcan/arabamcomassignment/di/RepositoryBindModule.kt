d8de6392dcecc3f3beb593225cefd20174c54cc1
Burak Can GORGULU <burak.gorgulu@beymen.com>
2023-10-14 00:43:36 +0300

    Arabamcom/Assignment arabamRepositort binding abstract fun added

--- app/src/main/java/com/bcan/arabamcomassignment/di/RepositoryBindModule.kt
+++ app/src/main/java/com/bcan/arabamcomassignment/di/RepositoryBindModule.kt
@@ -1,5 +1,8 @@
 package com.bcan.arabamcomassignment.di
 
+import com.bcan.arabamcomassignment.data.repository.ArabamRepository
+import com.bcan.arabamcomassignment.data.repository.ArabamRepositoryImpl
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
@@ -7,4 +10,9 @@ import dagger.hilt.android.components.ViewModelComponent
 @Module
 @InstallIn(ViewModelComponent::class)
 abstract class RepositoryBindModule {
+    @Binds
+    abstract fun bindArabamRepository(
+        arabamRepositoryImpl: ArabamRepositoryImpl
+    ): ArabamRepository
+
 }
