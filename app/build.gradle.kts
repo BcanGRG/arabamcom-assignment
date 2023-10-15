a37143b557b8939ebfe25a938dcf5453d1784ac2
Burak Can GORGULU <burak.gorgulu@beymen.com>
2023-10-15 11:31:06 +0300

    Arabamcom/Assignment added navigation and material libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,8 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation ("androidx.compose.material:material:1.4.3")
+    implementation ("androidx.compose.material:material-icons-extended:1.4.3")
     implementation("androidx.compose.material3:material3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -74,6 +76,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
+    implementation("androidx.navigation:navigation-compose:2.6.0")
 
     // Hilt
     implementation("com.google.dagger:hilt-android:2.44")
